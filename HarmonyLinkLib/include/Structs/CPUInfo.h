7f92fa6512505650dbb701662bae1c6822b7de98
Jordon Brooks <jordonbc@hotmail.co.uk>
2024-05-22 18:55:45 +0100

    Add CPUInfo struct

---
+++ HarmonyLinkLib/include/Structs/CPUInfo.h
@@ -0,0 +1,32 @@
+// Copyright (c) 2024 Jordon Brooks
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <stdbool.h>
+
+#include "Core.h"
+
+typedef struct
+{
+    char* VendorID;
+    bool Model_Name;
+    unsigned int Physical_Cores;
+    unsigned int Logical_Cores;
+    char[] Flags;
+
+} FCPUInfo;
+
+//HARMONYLINKLIB_API void FBattery_print(const FCPUInfo* self);
+
